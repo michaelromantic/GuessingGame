45e17b06afea1b78969d487b0061d8f3428b0920
Trent Thomas <trentthomas123456789@gmail.com>
2021-01-26 13:49:13 -0500

    Added negative 1 feature

--- Program.cs
+++ Program.cs
@@ -17,15 +17,22 @@ namespace GuessingGame
 
             if (userNumber == testNumber)
                 Console.WriteLine("You Won!");
-            
-            else if (userNumber != testNumber && userNumber != 0)
+
+            else if (userNumber != testNumber && userNumber != 0 && userNumber != -1)
                 Console.WriteLine("Try Again.");
 
-            else  if (userNumber == 0)
+            else if (userNumber == 0)
             {
                 Console.WriteLine("Please Guess a number between 1-10.");
                 Console.WriteLine("If it is right then you win, if not then you lose.");
             }
-        }
+            else if (userNumber == -1) 
+            {
+               Console.WriteLine("That is does not follow the rules, Goodbye.");
+                
+            }
+
+        }   
+
     }
 }
