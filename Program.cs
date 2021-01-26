36bb1c15626c18f935b8902e9e637c15977a484b
Trent Thomas <trentthomas123456789@gmail.com>
2021-01-26 12:00:50 -0500

    Hopefully This Works

--- Program.cs
+++ Program.cs
@@ -7,6 +7,8 @@ namespace GuessingGame
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            Console.WriteLine("Hopefully This Works");
         }
+
     }
 }
