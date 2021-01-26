fd5c7c0a643a1a9b82d241e4b593a9148db78217
Michael Romantic <michaeljromantic@gmail.com>
2021-01-26 13:33:46 -0500

    started loop

--- Program.cs
+++ Program.cs
@@ -6,15 +6,26 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            //Console.WriteLine("Hello World!");
-            //Console.WriteLine("Hopefully This Works");
-            //Console.WriteLine("Hopefully this DOESN'T WORK!");
             Console.WriteLine("Welcome!");
             Console.WriteLine("Please Guess a number between 1-10.");
             Console.WriteLine("If it is right then you win, if not then you lose.");
+
+            int userNumber = Convert.ToInt32(Console.ReadLine());
             Random number = new Random();
-            int randonNumber = number.Next(1, 10);
-            Console.WriteLine(randonNumber);
+            //int randomNumber = number.Next(1, 10);
+            int testNumber = 7;
+
+            if (userNumber == testNumber)
+                Console.WriteLine("You Won!");
+            
+            else if (userNumber != testNumber && userNumber != 0)
+                Console.WriteLine("Try Again.");
+
+            else  if (userNumber == 0)
+            {
+                Console.WriteLine("Please Guess a number between 1-10.");
+                Console.WriteLine("If it is right then you win, if not then you lose.");
+            }
         }
     }
 }
