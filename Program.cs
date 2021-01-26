ddb91b368d2285222e02b99d349e4d3895b7c107
Trent Thomas <trentthomas123456789@gmail.com>
2021-01-26 13:22:55 -0500

    Welcome text inserted

--- Program.cs
+++ Program.cs
@@ -9,9 +9,11 @@ namespace GuessingGame
             //Console.WriteLine("Hello World!");
             //Console.WriteLine("Hopefully This Works");
             //Console.WriteLine("Hopefully this DOESN'T WORK!");
-
+            Console.WriteLine("Welcome!");
+            Console.WriteLine("Please Guess a number between 1-10.");
+            Console.WriteLine("If it is right then you win, if not then you lose.");
             Random number = new Random();
-            int randonNumber = number.Next(1, 500);
+            int randonNumber = number.Next(1, 10);
             Console.WriteLine(randonNumber);
         }
     }
