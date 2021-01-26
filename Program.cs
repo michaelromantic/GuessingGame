c0a60a4ee12585aa649086f27881e48347ba0172
Michael Romantic <michaeljromantic@gmail.com>
2021-01-26 13:14:21 -0500

    random number

--- Program.cs
+++ Program.cs
@@ -6,10 +6,13 @@ namespace GuessingGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("Hopefully This Works");
-            Console.WriteLine("Hopefully this DOESN'T WORK!");
-        }
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("Hopefully This Works");
+            //Console.WriteLine("Hopefully this DOESN'T WORK!");
 
+            Random number = new Random();
+            int randonNumber = number.Next(1, 500);
+            Console.WriteLine(randonNumber);
+        }
     }
 }
